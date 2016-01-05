bdcad4dad24f76e1f82f7f9d45f31902ef71f124
Martin Törnqvist <hello.martintornqvist@gmail.com>
2016-01-05 22:22:42 +0100

    Added scoreboard

--- src/gameplay.js
+++ src/gameplay.js
@@ -3,13 +3,23 @@
  */
 megamanGame.Game = function(game){
 
+    var score = 0;
+    var scoreText;
+    var scoreString = '';
+
 };
 
 megamanGame.Game.prototype = {
     create: function(){
         this.background = this.add.sprite(0,0, 'gameBackground');
+
+        //In game music
         this.music = this.add.audio('gameMusic');
         this.music.play();
+
+        //Scoreboard
+        scoreText = this.add.text(16,16, 'Score: 0', {fontSize:'24px', fill:'#fff'});
+        scoreString = 'Score: ';
     },
 
     update: function(){
